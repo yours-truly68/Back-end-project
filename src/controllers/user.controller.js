231eab1ecd8b170e772a83672b86be0ebd31163e
yours-truly68 <mohammedrazim880@gmail.com>
2025-07-29 07:42:05 +0530

    add register and bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,72 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
+import uploadOnCloudinary from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "ok",
+  //get user data from frontend
+  const { fullName, email, username, password } = req.body;
+  console.log("email: ", email);
+
+  //validation -not empty
+
+  //   if (fullName === "") {
+  //     throw new ApiError(400, "Full Name is required");
+  //   }
+
+  if (
+    [fullName, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+  //check if user already exists - with username and email
+  const existedUser = User.findOne({
+    $or: [{ username }, { email }],
   });
+  if (existedUser) {
+    throw new ApiError(409, "User with email or username already exists");
+  }
+
+  //check for images avatar
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  console.log(req.files);
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar is required");
+  }
+
+  //upload them to cloudinary, avatar uploaded?
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+  if (!avatar) {
+    throw new ApiError(400, "Avatar is required");
+  }
+  //create user object - create call in db
+
+  const user = await User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username: username.toLowerCase(),
+  });
+  //remove password and refresh token feed from response
+  //check for user creation
+  const createdUser = await User.findById(user._id).select(
+    "-password",
+    "-refreshToken" //only items u dont need,
+  );
+
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wrong while registering the user");
+  }
+  //return response? response : error
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "user registered successfully"));
 });
 
 export { registerUser };
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,4 @@
-import mongoose, { mongoose, Schema } from "mongoose";
+import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
@@ -53,7 +53,7 @@ const userSchema = new Schema(
 
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return;
-  this.password = bcrypt.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
