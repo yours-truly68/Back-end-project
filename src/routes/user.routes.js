96cf015c9f48fafc1234c8c25eccda7034fd0d2d
yours-truly68 <mohammedrazim880@gmail.com>
2025-08-14 15:17:46 +0530

    feat: add user account management routes for password change, current user retrieval, and avatar/cover image updates

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,13 @@ import {
   logoutUser,
   registerUser,
   refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { veryJWT } from "../middlewares/auth.middleware.js";
@@ -30,5 +37,23 @@ router.route("/login").post(loginUser);
 router.route("/logout").post(veryJWT, logoutUser);
 
 router.route("/refresh-token").post(refreshAccessToken);
+
+router.route("/change-password").post(veryJWT, changeCurrentPassword);
+
+router.route("/current-user").get(veryJWT, getCurrentUser);
+
+router.route("/update-account").patch(updateAccountDetails);
+
+router
+  .route("/avatar")
+  .patch(veryJWT, upload.single("avatar"), updateUserAvatar);
+
+router
+  .route("/cover-image")
+  .patch(veryJWT, upload.single("/coverImage"), updateUserCoverImage);
+
+router.route("/c/:username").get(veryJWT, getUserChannelProfile);
+
+router.route("/history").get(veryJWT, getWatchHistory);
 
 export default router;
